1ace5d7275e2413271c8e35aae94e8a7edf3f886
jose-r-a-rodriguez <jose.r.a.rodriguez@accenture.com>
2026-01-15 20:21:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from core.excel_writer import append_rows_to_sheet
+from core.mapper import classify_table
+
 import streamlit as st
 import tempfile
 from core.docx_extractor import extract_all_tables
@@ -21,3 +24,45 @@ if uploaded:
         for i, t in enumerate(tables[:3]):
             st.write(f"Table {i+1}")
             st.dataframe(t)
+
+mapping_df = None
+
+for df in tables:
+    if classify_table(df) == "table_mapping":
+        mapping_df = df
+        break
+
+if mapping_df is None:
+    st.error("Could not find Table Mapping section")
+    st.stop()
+
+rows = []
+
+for _, r in mapping_df.iterrows():
+    row = {
+        "data_entity": None,  # optional for MVP
+        "source_field": r.get("Source Field") or r.get("Source Column"),
+        "raw_table": r.get("Raw Table"),
+        "raw_column": r.get("Raw Column"),
+        "bronze_table": r.get("Bronze Table"),
+        "bronze_column": r.get("Bronze Column"),
+    }
+    rows.append(row)
+
+template_path = "templates/Master_Mapping_Template.xlsx"
+output_path = "Generated_Mapping.xlsx"
+
+append_rows_to_sheet(
+    template_path=template_path,
+    output_path=output_path,
+    sheet_name="Source to Raw to Bronze",
+    rows=rows
+)
+with open(output_path, "rb") as f:
+    st.download_button(
+        label="Download Excel Mapping",
+        data=f,
+        file_name="Generated_Mapping.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
